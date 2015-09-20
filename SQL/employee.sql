8f66b84015882f7bc6ba46d6b06e6b1698d50270
Sriram <srirambtecit@gmail.com>
2015-09-21 01:53:30 +0530

    query updated with meaningful data with output

--- SQL/employee.sql
+++ SQL/employee.sql
@@ -20,35 +20,35 @@ from emp where (
 
 #SAMPLE OUTPUT:-
 mysql> select * from emp order by dept_id;
-+------+----------+--------+---------+
-| id   | name     | salary | dept_id |
-+------+----------+--------+---------+
-|    1 | sriram1  |  10000 |       1 |
-|    4 | sriram4  |   3000 |       1 |
-|    5 | sriram5  |   5000 |       1 |
-|    9 | sriram9  |   4000 |       2 |
-|    6 | sriram6  |   6000 |       2 |
-|    2 | sriram2  |   9000 |       2 |
-|   10 | sriram10 |  11000 |       2 |
-|    3 | sriram3  |   5000 |       3 |
-|    7 | sriram7  |   7000 |       3 |
-|    8 | sriram8  |   2000 |       3 |
-+------+----------+--------+---------+
++------+-----------+--------+---------+
+| id   | name      | salary | dept_id |
++------+-----------+--------+---------+
+|    1 | sriram    |  10000 |       1 |
+|    4 | prabhakar |   3000 |       1 |
+|    5 | kamal     |   5000 |       1 |
+|    9 | sundar    |   4000 |       2 |
+|    6 | siva      |   6000 |       2 |
+|    2 | ashok     |   9000 |       2 |
+|   10 | ramesh    |  11000 |       2 |
+|    3 | praveen   |   5000 |       3 |
+|    7 | surya     |   7000 |       3 |
+|    8 | gokul     |   2000 |       3 |
++------+-----------+--------+---------+
 10 rows in set (0.00 sec)
 
 mysql> select name, dept_id, salary from emp where ( select count(*) from emp as e where e.dept_id=emp.dept_id and emp.salary <= e.salary order by e.salary desc ) <= 3 order by dept_id;
-+----------+---------+--------+
-| name     | dept_id | salary |
-+----------+---------+--------+
-| sriram1  |       1 |  10000 |
-| sriram4  |       1 |   3000 |
-| sriram5  |       1 |   5000 |
-| sriram2  |       2 |   9000 |
-| sriram6  |       2 |   6000 |
-| sriram10 |       2 |  11000 |
-| sriram3  |       3 |   5000 |
-| sriram7  |       3 |   7000 |
-| sriram8  |       3 |   2000 |
-+----------+---------+--------+
++-----------+---------+--------+
+| name      | dept_id | salary |
++-----------+---------+--------+
+| sriram    |       1 |  10000 |
+| prabhakar |       1 |   3000 |
+| kamal     |       1 |   5000 |
+| ashok     |       2 |   9000 |
+| siva      |       2 |   6000 |
+| ramesh    |       2 |  11000 |
+| praveen   |       3 |   5000 |
+| surya     |       3 |   7000 |
+| gokul     |       3 |   2000 |
++-----------+---------+--------+
 9 rows in set (0.00 sec)
 
